9951fbfcbd00afe99bf4bf51e34e3a43a17adce0
Raphael Mutili <kqraphael@yahoo.com>
2022-02-21 13:27:24 +0300

    Create main.py

--- Code/main.py
+++ Code/main.py
@@ -1,2 +1,7 @@
 ## Learning Python
 print("python is a cool development code")
+print(2)
+print(20*24*60)
+print("20 days are " + str(50) + " minutes")
+print(f"20 days are {20 * 24 * 60} minutes") ## Another way of converting integer to string inside a string statement (Add f' before statement and enclose integer with {} == Works form python 3.6
+
