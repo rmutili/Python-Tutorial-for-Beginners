d50ab05ae8935c7e4f65312dd4d17288d688bad4
Raphael Mutili <kqraphael@yahoo.com>
2022-02-21 13:32:59 +0300

    Create Variables.py

---
+++ Code/Variables.py
@@ -0,0 +1,12 @@
+## Learning Python
+print("python is a cool development code")
+print(2)
+print(20*24*60)
+print("20 days are " + str(50) + " minutes")
+print(f"20 days are {20 * 24 * 60} minutes") ## Another way of converting integer to string inside a string statement (Add f' before statement and enclose integer with {} == Works form python 3.6
+print(f"30 days are {30 * 24 * 60} minutes")
+print(f"40 days are {40 * 24 * 60} minutes")
+print(f"50 days are {50 * 24 * 60} minutes")
+
+calculation_to_seconds = 24 * 60 * 60 ## Variable to convert to seconds
+print(f"20 days are {20 * calculation_to_seconds} seconds")
