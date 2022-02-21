900953104013fc6fa47c6cbaea628e45068ff443
Raphael Mutili <kqraphael@yahoo.com>
2022-02-21 16:51:27 +0300

    Create functions.py

---
+++ Code/functions.py
@@ -0,0 +1,9 @@
+## Learning Python
+calculation_to_units = 24 ## Variable to convert to units
+name_of_unit = "hours"
+
+def days_to_units():
+    print(f"20 days are {20 * calculation_to_units} {name_of_unit}")
+    print("All good!")
+
+days_to_units() ## When you create a function, you have to use or call
